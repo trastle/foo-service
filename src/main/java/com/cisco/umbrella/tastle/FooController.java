f5fd93f500b1b331171eae797f37afc8b763f4ae
Troy Astle <tastle@cisco.com>
2017-06-26 14:25:56 +0100

    Cleanup unused atomic long

--- src/main/java/com/cisco/umbrella/tastle/FooController.java
+++ src/main/java/com/cisco/umbrella/tastle/FooController.java
@@ -1,13 +1,10 @@
 package com.cisco.umbrella.tastle;
 
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FooController {
-
-    private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/foo")
     public FooResponse greeting() {
